f391645768a14f83df8e45831509df91f4632901
root <root@localhost.localdomain>
2018-03-13 23:15:30 -0400

    bad algorithm with list pixel

--- code/GenerateData.py
+++ code/GenerateData.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def main():
     base_image_width, base_image_height = 320, 240
-    background_images_dir = '/root/Desktop/Dataset/iccv09Data/images/'    
+    background_images_dir = '/root/Downloads/iccv09Data/images/'    
     image_path_list, num_width, num_height, num_x, num_y = [], [], [], [], []
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
@@ -14,56 +14,62 @@ def main():
     for file in tqdm(os.listdir(background_images_dir)):
         image_path_list.append(os.path.join(background_images_dir, file))
     
+    counter = 0
     print("Generate images")
     for imagePath in tqdm(image_path_list):
         image = cv2.imread(imagePath, 0)
-        numbers_of_nums = np.random.uniform(1, 6)
+        image = cv2.resize(image, (base_image_width, base_image_height))
+        numbers_of_nums = np.random.randint(1, 6)#uniform(1, 6)
+        number_of_nums, num_height, num_width, num_x, num_y = checkOnSame(numbers_of_nums, base_image_width, base_image_height)
 
-        for nums in numbers_of_nums:
-            num_width.append(np.random.uniform(20, 100))
-            num_height.append(np.random.uniform(20, 100))
-            num_x.append(np.random.uniform(0, base_image_width - num_width[nums]))
-            num_y.append(np.random.uniform(0, base_image_height - num_height[nums]))
+        num_x = np.asarray(num_x, dtype = int)
+        num_y = np.asarray(num_y, dtype = int)
+        num_height = np.asarray(num_height, dtype = int)
+        num_width = np.asarray(num_width, dtype = int)
+
+        for i in range(number_of_nums):
+            image[num_y[i] : num_y[i] + num_height, num_x[i] : num_x[i] + num_width[i] ] = np.asarray(X_train[counter])
+            counter += 1
+            cv2.imwrite('/root/Desktop/DeepLearning/Faster-RCNN/code/RPNDataset/train/X/'+counter+'.jpg', image)
         
-        flags, pixel_arr, arr_one, arr_two = [], [], [], []
-        for nums in numbers_of_nums:
-            for j  in range(num_width[nums]):
+def checkOnSame(numbers_of_nums, base_image_width, base_image_height):
 
-                for i in range(num_height[nums]):
-                    arr_one.append(num_y[nums] + i)
-
-                arr_two.append(arr_one)
-            pixel_arr.append(arr_two)
-
-        #if numbers have intersections on the base image remove them from the list
-        for nums in numbers_of_nums:
-            for i in numbers_of_nums:
-                if(i == nums):
-                    continue
-                for pix_x in range(num_width[nums]):
-                    for pix_y in range(num_height[nums]):
-                        if(pixel_arr[nums][pix_x][pix_y] == pixel_arr[i][pix_x][pix_y]):
-                            flags.append(nums)
-       
-        for i in numbers_of_nums:
-           if i in flags: 
-               num_height[i] = -1
-               num_width[i] = -1
-               num_x = -1
-               num_y = -1  
-                                                  
-        for i in numbers_of_nums:
-            
-            if num_width[i] == -1 and i < np.shape(num_height):
-                num_width.remove(i)
-                num_height.remove(i)
-                num_x.remove(i)
-                num_y.remove(i)
+    num_height, num_width, num_x, num_y = [], [], [], []
+    for nums in range(numbers_of_nums):
+            num_width.append(np.random.randint(20, 50))
+            num_height.append(np.random.randint(20, 50))
+            num_x.append(np.random.randint(0, base_image_width - num_width[nums]))
+            num_y.append(np.random.randint(0, base_image_height - num_height[nums]))
         
+    #watch list impliment
+    flags, pixel_arr, arr_one, arr_two = [], [], [], []
+    for nums in range(numbers_of_nums):
+        for j  in range(num_width[nums]):
 
+            for i in range(num_height[nums]):
+                arr_one.append(num_y[nums] + i)
+
+            arr_two.append(arr_one)
+        pixel_arr.append(arr_two)
     
     
-   
+    #if numbers have intersections on the base image remove them from the list
+    for nums in range(numbers_of_nums):
+        for i in range(nums + 1, numbers_of_nums):
+            if(i == nums):
+                continue
+            for pix_x in range(num_width[nums]):
+                for pix_y in range(num_height[nums]):
+                    if(pixel_arr[nums][pix_x][pix_y] == pixel_arr[i][pix_x][pix_y]):
+                        flags.append(nums)
+    
+    for i in range(numbers_of_nums):
+        num_height = [i for j,i in enumerate(num_height) if j not in flags]
+        num_width = [i for j,i in enumerate(num_width) if j not in flags]
+        num_x = [i for j,i in enumerate(num_x) if j not in flags]
+        num_y = [i for j,i in enumerate(num_y) if j not in flags]
+    numbers_of_nums = len(num_x)
+    return numbers_of_nums, num_height, num_width, num_x, num_y
 
 
 if __name__ =="__main__":
